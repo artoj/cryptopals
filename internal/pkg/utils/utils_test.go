5c397ac43e78d3ac4e311bfd47513e3109bd1796
Arto Jonsson <artoj@users.noreply.github.com>
2019-03-20 12:09:57 +0200

    rewrite and add some tests

--- internal/pkg/utils/utils_test.go
+++ internal/pkg/utils/utils_test.go
@@ -2,22 +2,61 @@ package utils
 
 import "testing"
 
-func TestZeroPadd(t *testing.T) {
+func isEqual(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if b[i] != v {
+			return false
+		}
+	}
+	return true
+}
+
+func isEqualMulti(a, b [][]byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if !isEqual(b[i], v) {
+			return false
+		}
+	}
+	return true
+}
+
+func TestTranspose(t *testing.T) {
+	tables := []struct {
+		input    [][]byte
+		expected [][]byte
+	}{
+		{[][]byte{[]byte{0xff, 0x00, 0xff}, []byte{0x00, 0xff, 0x00}}, [][]byte{[]byte{0xff, 0x00}, []byte{0x00, 0xff}, []byte{0xff, 0x00}}},
+	}
+	for _, table := range tables {
+		transposed := Transpose(table.input)
+		if !isEqualMulti(transposed, table.expected) {
+			t.Errorf("Transpose of %v was incorrect, got: %v, want: %v", table.input, transposed, table.expected)
+		}
+	}
+}
+
+func TestZeroPad(t *testing.T) {
 	tables := []struct {
 		input     []byte
 		blocksize int
-		paddedLen int
+		expected  []byte
 	}{
-		{[]byte("This is a test ab"), 8, 24},
-		{[]byte("This is a test 1"), 8, 16},
-		{[]byte("This is a test"), 8, 16},
-		{[]byte("abcdefgh"), 8, 8},
-		{[]byte("abc"), 8, 8},
+		{[]byte("This is a test ab"), 8, []byte("This is a test ab\x00\x00\x00\x00\x00\x00\x00")},
+		{[]byte("This is a test 1"), 8, []byte("This is a test 1")},
+		{[]byte("This is a test"), 8, []byte("This is a test\x00\x00")},
+		{[]byte("abcdefgh"), 8, []byte("abcdefgh")},
+		{[]byte("abc"), 8, []byte("abc\x00\x00\x00\x00\x00")},
 	}
 	for _, table := range tables {
 		padded := ZeroPad(table.input, table.blocksize)
-		if len(padded) != table.paddedLen {
-			t.Errorf("ZeroPad of (\"%s\", %d) was incorrect, got: %d, want: %d", table.input, table.blocksize, len(padded), table.paddedLen)
+		if !isEqual(padded, table.expected) {
+			t.Errorf("ZeroPad of (\"%s\", %d) was incorrect, got: %q, want: %q", table.input, table.blocksize, padded, table.expected)
 		}
 	}
 }
